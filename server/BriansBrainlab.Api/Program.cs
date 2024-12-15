f124551286fabb11e359fb6d69d499e4a8738702
Justin Drögemeier <justindroegemeier@gmail.com>
2024-12-15 01:12:55 +0100

    feat: add openapi spec

--- server/BriansBrainlab.Api/Program.cs
+++ server/BriansBrainlab.Api/Program.cs
@@ -4,11 +4,22 @@ using BriansBrainlab.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddOpenApi();
 builder.ConfigureOpenTelemetry();
 builder.ConfigureSharedServices();
 builder.AddBriansBrainlabFeatures();
 
 var app = builder.Build();
+
+app.MapOpenApi();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
 
 app.MapGet("/", () => "Hello World!");
 
--- server/BriansBrainlab.Features/User/EndpointBuilderExtensions.cs
+++ server/BriansBrainlab.Features/User/EndpointBuilderExtensions.cs
@@ -1,5 +1,6 @@
 using BriansBrainlab.Features.User.CreateUser;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
 namespace BriansBrainlab.Features.User;
@@ -9,7 +10,8 @@ public static class EndpointBuilderExtensions
     public static IEndpointRouteBuilder MapUserRoutes(this IEndpointRouteBuilder endpoints)
     {
         endpoints.MapGroup("/user")
-            .MapCreateUserEndpoint();
+            .MapCreateUserEndpoint()
+            .WithTags("user");
         
         return endpoints;
     }
